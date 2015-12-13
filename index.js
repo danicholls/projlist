606ed8c90c1c81ea12a588db6c38c9856687c8ef
danicholls <diasporate@yahoo.com>
2015-12-13 09:15:35 -0700

    basic errors

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 var projectList = { "projects" : [
     // projects will be added here
+    // { "project-name": "projName", "directory-name": "projName/", "project-silo": "otherProjNameOrDir" }
 ]};
 
 
 // exposed to cli
 module.exports = {
     /* add project */
-    add: function(proj){
-        console.log("adding"+ proj +"..");
+    add: function(projName,dirName,projSilo){
+        if ( !projName ) {
+            console.log("!! fatal error: Declare your project name (e.g. $ projlist add 'project name').");
+            return;
+        }
+        
+        if ( !dirName ) {
+            console.log("warning: No directory provided, so attempting to use the project name as the directory name, relative to the silo's directory (e.g., $gitlab/project).");
+        }
+        console.log("adding"+ projName +"..");
     },
     /* remove project */
     rm: function(proj){
